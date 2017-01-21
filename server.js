5acbc13d328e55eb0e37e6525f0de786590e54a2
Sergiu Moraru <tyler@100.0.168.192.in-addr.arpa>
2017-01-21 19:49:11 +0200

    op

---
+++ server.js
@@ -0,0 +1,42 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
+var {mongoose} = require('./server/db/mongoose');
+var {Todo} = require('./server/models/todo');
+var {User} = require('./server//models/user');
+var port = process.env.PORT || 3000;
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => res.status(400).send(err));
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send(); 
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) return res.status(404).send();
+        res.send({todo});
+    }).catch((e) => res.status(400).send());
+});
+
+app.listen(port, () => {console.log(`Started on port ${port}`);});
+
+module.exports = {app};
